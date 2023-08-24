66930d15d0103438c9d9ff2f8677a92e80a64106
joshdotjs <joshdotjs@gmail.com>
2023-08-24 06:26:21 -0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello');
+console.log(process.env);
+console.log('NODE_ENV: ', process.env.NODE_ENV);
+if (process.env.NODE_ENV !== 'production') {
+  // require('dotenv').config();
+  const dotenv = await import('dotenv'); // top level await / non-top level import 🤯
+  dotenv.config();
+}
+
+
+// const express = require('express');
+import express from 'express';
+
+// const cors = require('cors');
+import cors from 'cors';
+
+const server = express();
+
+// middleware
+server.use(express.json());
+
